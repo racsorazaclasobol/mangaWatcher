743308d1cd0263f909edf5daea4b0448d266e600
Oscar Alcazar <oscar.alcazar.lobos@gmail.com>
2023-02-03 13:46:58 -0300

    TitleMangas.jsx: Responsive design

--- src/mangawatcher/components/Public/TitleMangas.jsx
+++ src/mangawatcher/components/Public/TitleMangas.jsx
@@ -1,6 +1,7 @@
 import { Grid, IconButton, Tooltip, Typography } from "@mui/material";
-import { useMangaStore, useUIStore } from "../../hooks";
 import PinIcon from '@mui/icons-material/Pin';
+
+import { useMangaStore, useUIStore } from "../../hooks";
 import { ModalChapterSelector } from "./ModalChapterSelector";
 
 export const TitleMangas = () => {
@@ -15,23 +16,25 @@ export const TitleMangas = () => {
     }
 
     return (
-        <Grid container justifyContent='center' pb={ 2 } className="animate__animated animate__fadeIn">
+        <Grid container justifyContent='center'  textAlign='center' pb={ 2 } className="animate__animated animate__fadeIn">
 
-            <Grid item lg={ 7 } sm={ 9 } xs={ 10 } pl={ 13 } pr={ 3 } minHeight='50px' className="box-shadow" sx={{ borderTop: '1px solid', borderColor: `${ styleMode }.secondary` , borderBottomLeftRadius:'10px' }} bgcolor={ `${ styleMode }.primary` } alignItems='center' textAlign='center' >
-                
-				<Typography variant='h6' mt={ 1 } sx={{ color: `colors.gray.claro` }} >
-                    { `${ manga.nombre }  ${ capitulo } Español - ${ titulo } `}			
-				</Typography>
+            
+                <Grid item  xs={ 10 } p={ 1 } minHeight='50px' className="box-shadow" sx={{ paddingLeft: { md: '150px', sm: '75px', xs: '0px', lg: '250px',  } , borderTop: '1px solid', borderColor: `${ styleMode }.secondary` , borderBottomLeftRadius:'10px', backgroundColor: `${ styleMode }.primary` }} >
+                    
+                    <Typography variant='h6' mt={ 1 } sx={{ color: `colors.gray.claro` }} >
+                        { `${ manga.nombre }  ${ capitulo } Español - ${ titulo } `}			
+                    </Typography>
 
-            </Grid>
+                </Grid>
 
-            <Grid item lg={ 1 } sm={ 1 } xs={ 2 } minHeight='50px' className="box-shadow" sx={{ borderTop: '1px solid', borderColor: `${ styleMode }.secondary`, borderBottomRightRadius:'10px', display:'flex' }} bgcolor={ `${ styleMode }.primary` } alignItems='center' justifyContent='center' >
-                <Tooltip title='Capítulo'>
-                    <IconButton onClick={ onOpenModal }>
-                        <PinIcon sx={{ fontSize: '33px' }} className='botonesNavbar' />
-                    </IconButton>
-                </Tooltip>
-            </Grid>
+                <Grid item xs={ 2 } minHeight='50px' className="box-shadow" sx={{ borderTop: '1px solid', borderColor: `${ styleMode }.secondary`, borderBottomRightRadius:'10px', display:'flex' }} bgcolor={ `${ styleMode }.primary` } alignItems='center' justifyContent='center' >
+                    <Tooltip title='Capítulo'>
+                        <IconButton onClick={ onOpenModal }>
+                            <PinIcon sx={{ fontSize: '33px' }} className='botonesNavbar' />
+                        </IconButton>
+                    </Tooltip>
+                </Grid>
+           
 
             <ModalChapterSelector />
 
