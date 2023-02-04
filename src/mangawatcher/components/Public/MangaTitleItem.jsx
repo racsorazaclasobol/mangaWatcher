bf54f69ae007344cf3007ab6580a0a83857546b4
Oscar Alcazar <oscar.alcazar.lobos@gmail.com>
2023-02-04 18:25:25 -0300

    MangaTitleItem: Responsive Update

--- src/mangawatcher/components/Public/MangaTitleItem.jsx
+++ src/mangawatcher/components/Public/MangaTitleItem.jsx
@@ -1,5 +1,6 @@
 import { Grid, Card, CardContent, CardMedia, Typography, Box, Divider } from "@mui/material";
 import { useMangaStore, useUIStore } from "../../hooks";
+import { Breakpoints } from "./Breakpoints";
 
 export const MangaTitleItem = () => {
 
@@ -7,36 +8,37 @@ export const MangaTitleItem = () => {
     const { styleMode } = useUIStore();
     
     const onOpenManga = ( mangaId ) => {
-       
         startObtenerUltimoCap( mangaId );
-        
     }
-
-
 
     return (
         <>
+            <Breakpoints />
             {
-                mangas.map( ( { uid, nombre, portada, autor }, index, array ) => (
-                    <Grid key={ index } item xs={ 12 } sm={ 6 } md={ 4 } xl={ 3.5 } onClick={ () => onOpenManga( uid, nombre ) } className="pointer" >
-                        <Card sx={{ display: 'flex', height: '200px', justifyContent: 'flex-start', backgroundColor: `${ styleMode }.bgColorCards` }}>
+                mangas.map( ( { uid, nombre, portada, autor, capitulo, titulo }, index, array ) => (
+                    <Grid container key={ index } item xs={ 12 } sm={ 10 } md={ 6 } lg={ 4 } xl={ 3.5 } onClick={ () => onOpenManga( uid, nombre ) } className="pointer" >
+                        <Card sx={{ display: '', height: '200px', width:'100%',  backgroundColor: `${ styleMode }.bgColorCards` }}>
                             <CardMedia
                                 component="img"
-                                sx={{ width: 180, float:'right', boxShadow: '0px 0 7px 2px #888' }}
+                                sx={{ maxWidth: { xs: 140, sm: 180, xl: 200 }, float:'left', boxShadow: '0px 0 7px 2px #888' }}
                                 image={ portada }
                                 alt=""
                             />
-                            <Box sx={{ display: 'flex', flexDirection: 'column' }} mt={ 2 }>
-                                <CardContent sx={{ flex: '1 0 auto' }} >
+                            <Box sx={{ display: 'flex', flexDirection: 'column' }} >
+                                <CardContent  >
 
-                                    <Typography component="div" variant="h5" color="text.secondary" sx={{ fontWeight: 'bold' }}>
+                                    <Typography variant="h6" color="text.secondary" sx={{ fontWeight: 'bold'  }}>
                                         { nombre }
                                     </Typography>
 
                                     <Divider />
+                                    <Box mt={ 1 }>
+                                        <Typography variant="subtitle2" color="text.secondary" > Capítulo: { capitulo } </Typography>
+                                        <Typography variant="subtitle2" color="text.secondary" noWrap> Título: { titulo } </Typography>
+                                    </Box>
                                 </CardContent>
-                                <CardContent>
-                                    <Typography variant="subtitle2" color="text.secondary" component="div">
+                                <CardContent sx={{ flex: '1 0 auto' }} mb={ 1 }>
+                                    <Typography noWrap variant="subtitle2" color="text.secondary">
                                         ~ { autor }
                                     </Typography>
                                 </CardContent>
