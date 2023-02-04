bbf7f36ec606b1647816404aef5172273b0513b0
Oscar Alcazar <oscar.alcazar.lobos@gmail.com>
2023-02-04 14:26:01 -0300

    Colors bug fixs

--- src/mangawatcher/components/Public/MangaTitleItem.jsx
+++ src/mangawatcher/components/Public/MangaTitleItem.jsx
@@ -1,15 +1,17 @@
 import { Grid, Card, CardContent, CardMedia, Typography, Box, Divider } from "@mui/material";
-import { useMangaStore } from "../../hooks";
+import { useEffect } from "react";
+import { useMangaStore, useUIStore } from "../../hooks";
 
 export const MangaTitleItem = () => {
 
     const { mangas, startObtenerUltimoCap } = useMangaStore();
+    const { styleMode } = useUIStore();
     
     const onOpenManga = ( mangaId ) => {
        
         startObtenerUltimoCap( mangaId );
         
-    }
+    }    
 
     return (
         <>
@@ -17,7 +19,7 @@ export const MangaTitleItem = () => {
                 mangas.map( ({ uid, nombre, portada, autor }) => (
                     <>
                         <Grid key={ uid } item xs={ 12 } sm={ 6 } md={ 4 } xl={ 3.5 } onClick={ () => onOpenManga( uid, nombre ) } className="pointer" >
-                            <Card sx={{ display: 'flex', height: '200px', justifyContent: 'flex-start' }}>
+                            <Card sx={{ display: 'flex', height: '200px', justifyContent: 'flex-start', backgroundColor: `${ styleMode }.bgColorCards` }}>
                                 <CardMedia
                                     component="img"
                                     sx={{ width: 180, float:'right', boxShadow: '0px 0 7px 2px #888' }}
