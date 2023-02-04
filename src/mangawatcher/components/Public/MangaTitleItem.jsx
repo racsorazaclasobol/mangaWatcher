81cdfde3945a6b4383faecc291161dd7e7e00dc1
Oscar Alcazar <oscar.alcazar.lobos@gmail.com>
2023-02-04 17:12:34 -0300

    MangaTitleItem: Bug Fix, Key

--- src/mangawatcher/components/Public/MangaTitleItem.jsx
+++ src/mangawatcher/components/Public/MangaTitleItem.jsx
@@ -1,5 +1,4 @@
 import { Grid, Card, CardContent, CardMedia, Typography, Box, Divider } from "@mui/material";
-import { useEffect } from "react";
 import { useMangaStore, useUIStore } from "../../hooks";
 
 export const MangaTitleItem = () => {
@@ -11,39 +10,39 @@ export const MangaTitleItem = () => {
        
         startObtenerUltimoCap( mangaId );
         
-    }    
+    }
+
+
 
     return (
         <>
             {
-                mangas.map( ({ uid, nombre, portada, autor }) => (
-                    <>
-                        <Grid key={ uid } item xs={ 12 } sm={ 6 } md={ 4 } xl={ 3.5 } onClick={ () => onOpenManga( uid, nombre ) } className="pointer" >
-                            <Card sx={{ display: 'flex', height: '200px', justifyContent: 'flex-start', backgroundColor: `${ styleMode }.bgColorCards` }}>
-                                <CardMedia
-                                    component="img"
-                                    sx={{ width: 180, float:'right', boxShadow: '0px 0 7px 2px #888' }}
-                                    image={ portada }
-                                    alt=""
-                                />
-                                <Box sx={{ display: 'flex', flexDirection: 'column' }} mt={ 2 }>
-                                    <CardContent sx={{ flex: '1 0 auto' }} >
+                mangas.map( ( { uid, nombre, portada, autor }, index, array ) => (
+                    <Grid key={ index } item xs={ 12 } sm={ 6 } md={ 4 } xl={ 3.5 } onClick={ () => onOpenManga( uid, nombre ) } className="pointer" >
+                        <Card sx={{ display: 'flex', height: '200px', justifyContent: 'flex-start', backgroundColor: `${ styleMode }.bgColorCards` }}>
+                            <CardMedia
+                                component="img"
+                                sx={{ width: 180, float:'right', boxShadow: '0px 0 7px 2px #888' }}
+                                image={ portada }
+                                alt=""
+                            />
+                            <Box sx={{ display: 'flex', flexDirection: 'column' }} mt={ 2 }>
+                                <CardContent sx={{ flex: '1 0 auto' }} >
 
-                                        <Typography component="div" variant="h5" color="text.secondary" sx={{ fontWeight: 'bold' }}>
-                                            { nombre }
-                                        </Typography>
+                                    <Typography component="div" variant="h5" color="text.secondary" sx={{ fontWeight: 'bold' }}>
+                                        { nombre }
+                                    </Typography>
 
-                                        <Divider />
-                                    </CardContent>
-                                    <CardContent>
-                                        <Typography variant="subtitle2" color="text.secondary" component="div">
-                                            ~ { autor }
-                                        </Typography>
-                                    </CardContent>
-                                </Box>
-                            </Card>
-                        </Grid>
-                    </>
+                                    <Divider />
+                                </CardContent>
+                                <CardContent>
+                                    <Typography variant="subtitle2" color="text.secondary" component="div">
+                                        ~ { autor }
+                                    </Typography>
+                                </CardContent>
+                            </Box>
+                        </Card>
+                    </Grid>
                 ))
             }
         </>
