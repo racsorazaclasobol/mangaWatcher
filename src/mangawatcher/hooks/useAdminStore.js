52d0de74732a9e290d18bf3f805b59942da05ee4
Oscar Alcazar <oscar.alcazar.lobos@gmail.com>
2023-02-08 23:00:10 -0300

    useAdminstore: Updated

--- src/mangawatcher/hooks/useAdminStore.js
+++ src/mangawatcher/hooks/useAdminStore.js
@@ -1,17 +1,25 @@
 import { useDispatch, useSelector } from "react-redux";
+import Swal from "sweetalert2";
+
 import mangaApi from "../../api/mangaApi";
-import { onCreating, onSetNewChapter, onCreatedDone, onClearStore, onLoading, onSetInfoManga, onSetMangaTitles } from "../../store/admin/adminSlice";
+import { onCreating, onSetNewChapter, onCreatedDone, onClearStore, onLoading, onSetInfoManga, onSetMangaTitles, onSetChapetrsTitles, onSetErrorMessage } from "../../store/admin/adminSlice";
 
 export const useAdminStore = () => {
 
     const dispatch = useDispatch();
 
-    const { listMangaTitles, newChapter, message, isCreating, isLoading, infoManga } = useSelector( state => state.admin );
+    const { listMangaTitles, listChaptersTitles, newChapter, message, errorMessage, isCreating, isLoading, infoManga } = useSelector( state => state.admin );
     const { mangas: mangaList } = useSelector( state => state.manga );
 
 
     const startClearStore = () => {
         dispatch ( onClearStore() );
+    }
+
+    const startReportarError = ( ok, title, msg, type ) => {
+
+        dispatch( onSetErrorMessage( { ok, title, msg } ) );
+
     }
 
     /* **************************************** */
@@ -24,22 +32,63 @@ export const useAdminStore = () => {
 
     }
 
+    const startObtenerChapterPorUID = async( chapterUid = '' ) => {
+
+        try {
+
+            dispatch( onLoading() );
+
+            const { data: chapter } = await mangaApi.get( `/chapters/byid/${ chapterUid }` );
+            
+            dispatch( onSetInfoManga( chapter ) );
+            
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
     //TODO: Validaciones
+    const startObtenerListaCapitulos = async( mangaUid = '' ) => {
+
+        dispatch( onLoading() );
+
+        const { data } = await mangaApi.get( `/chapters/list/${ mangaUid }` );
+
+        dispatch( onSetChapetrsTitles( data ) )
+
+    }
+
+    const startUpdateChapter = async( chapter ) => {
+
+        dispatch( onLoading() );
+
+        const { uid, ...chapterToUpdate } = chapter;
+        const { paginas } = chapterToUpdate;
+
+        const { data } = await mangaApi.put( `/${ uid }`, chapterToUpdate )
+
+    }
+
     const startSaveChapter = async ( files = [] ) => {
 
-        dispatch( onCreating( true ) );
+        dispatch( onLoading() );
         
         try {
+
+            if( files.length <= 4 ) return dispatch( onSetErrorMessage({ ok: false, title: 'Hay un problema con su publicación', msg: 'Debe subir al menos 5 páginas', type: 'warning' }) )
         
             const chapterToSave = { ...newChapter };
             
             let formData = new FormData();
             
             const { data } = await mangaApi.post( '/chapters/', chapterToSave );
+
+            if( !data ) return onSetErrorMessage({ ok: false, title: 'Ha ocurrido un error', msg: 'Ha ocurrido un error subiendo el capítulo', type: 'error' })
             
             for ( const fileSelected of files ) {
                 const { file } = fileSelected;
-                formData.append( 'archivo', file );                
+                formData.append( 'archivo', file );
             }
             
             await mangaApi.put( `/uploads/${ data.uid }`, formData, { headers: { 'Content-Type': 'multipart/form-data' } } );
@@ -47,10 +96,37 @@ export const useAdminStore = () => {
             dispatch( onCreatedDone('Capítulo agregado correctamente.') );
             
         } catch (error) {
-            console.log(error)
-            
+            const { response }  = error;
+            const { data }      = response;
+
+            onSetErrorMessage({ ok: false, title: 'Ha ocurrido un error', msg: data.msgError, type: 'error' })
         }        
     }
+
+    //TODO: Validaciones
+    const startObtenerUltimoCap = async ( mangaId = '' ) => {
+
+        try {
+            
+            if( mangaId === '' ) return;
+    
+            dispatch( onLoading() );
+            dispatch( onResetCounter());
+            
+            const { data: chapter } = await mangaApi.get( `/chapters/last/${ mangaId }` );
+
+            dispatch( onSetInfoManga( chapter ) );
+
+        } catch (error) {
+            console.log( error );
+            //TODO: Mejorar manejo del error
+        }
+
+    }
+
+    /* **************************************** */
+    /*            MANEJO DE MANGAS              */
+    /* **************************************** */
 
     //TODO: Validaciones
     const startObtenerTitulosMangas = async() => {
@@ -81,31 +157,6 @@ export const useAdminStore = () => {
         }
 
     }
-
-    //TODO: Validaciones
-    const startObtenerUltimoCap = async ( mangaId = '' ) => {
-
-        try {
-            
-            if( mangaId === '' ) return;
-    
-            dispatch( onLoading() );
-            dispatch( onResetCounter());
-            
-            const { data: chapter } = await mangaApi.get( `/chapters/last/${ mangaId }` );
-
-            dispatch( onSetInfoManga( chapter ) );
-
-        } catch (error) {
-            console.log( error );
-            //TODO: Mejorar manejo del error
-        }
-
-    }
-
-    /* **************************************** */
-    /*            MANEJO DE MANGAS              */
-    /* **************************************** */
 
     //TODO: Validaciones
     const startSaveManga = async ( manga ) => {
@@ -187,16 +238,22 @@ export const useAdminStore = () => {
         infoManga,
         isCreating,
         isLoading,
+        listChaptersTitles,
+        listMangaTitles,
         mangaList,
         message,
-        listMangaTitles,
+        errorMessage,
 
         //Metodos y Funciones
         startClearStore,
+        startReportarError,
+        startObtenerChapterPorUID,
+        startObtenerListaCapitulos,
         startObtenerTitulosMangas,
         startObtenerUltimoCap,
         startSaveChapter,
         startStoreNuevoCapitulo,
+        startUpdateChapter,
 
         startSaveManga,
         startUpdateManga,
