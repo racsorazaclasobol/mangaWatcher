c23a74c9b73c53e94ba52fb7bf41c458d67f0eb2
Oscar Alcazar <oscar.alcazar.lobos@gmail.com>
2023-02-11 18:55:24 -0300

    Test bug fix for production on vercel

--- src/mangawatcher/routes/MangaRoutes.jsx
+++ src/mangawatcher/routes/MangaRoutes.jsx
@@ -7,12 +7,12 @@ export const MangaRoutes = () => {
 			
 			<Routes>
 
-				<Route path="home" element={ <MangaPage /> } />
+				<Route path="" element={ <MangaPage /> } />
 				<Route path="reading/:id" element={ <MangaPage /> } />
 				<Route path="/admin/managerManga/:action" element={ <MangaManager /> } />
 				<Route path="/admin/managerChapter/:action" element={ <ChapterManager /> } />
 
-				<Route path="/*" element={ <Navigate to="/home" /> } />
+				<Route path="/*" element={ <Navigate to="/" /> } />
 
 			</Routes>
 
