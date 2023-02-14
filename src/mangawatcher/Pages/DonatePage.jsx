dccabea1e78a9c6002cd41a1fda3a67905f9251a
Oscar Alcazar <oscar.alcazar.lobos@gmail.com>
2023-02-13 23:34:06 -0300

    Responsive fix

--- src/mangawatcher/Pages/DonatePage.jsx
+++ src/mangawatcher/Pages/DonatePage.jsx
@@ -6,6 +6,7 @@ import { MangaLayout } from "../layout/MangaLayout"
 import { useState } from "react";
 import { useUIStore } from "../hooks";
 import { color } from "@mui/system";
+import { Breakpoints } from "../components";
 
 export const DonatePage = () => {
 
@@ -30,89 +31,81 @@ export const DonatePage = () => {
 
     return (
         <MangaLayout>
-            <Grid container minHeight="calc(100vh - 230px)" p={ 5 } justifyContent="center" className="animate__animated animate__fadeIn">
+            <Grid container minHeight="calc(100vh - 230px)" mt={ 5 } mb={ 5 } justifyContent="center" className="animate__animated animate__fadeIn">
 
-                <Grid item xs={ 12 } md={ 10 } >
-                    <Typography variant="h4" sx={{ fontWeight:'bold', color: textColor }}>
-                        Ayudanos con una donación
-                    </Typography>
-                    <Typography variant="p" sx={{ color: textColor }}>
-                            Ayudanos a seguir ofreciendo un servicio gratuito y libre de publicidad.
-                    </Typography>
-                    <Divider />
-                </Grid>
+                {/* <Breakpoints /> */}
 
-                <Grid container direction={{ xs: 'row', md: 'row' }} justifyContent="center" mt={ 5 } >
-                    
-                    <Grid item xs={ 12 } md={ 5 }  >
+                <Grid item xs={ 10 } container>
 
-                        <Typography  variant="h6" color={ textColor }> Métodos de donación</Typography>
+                    <Grid item xs={ 12 } md={ 10 } >
 
-                        <hr className="hr hr-blurry" style={{ color: textColor, marginBottom: '20px' }}/> 
+                        <Typography variant={ "h5" } sx={{ fontWeight:'bold', color: textColor }} >
+                            Ayudanos con una donación
+                        </Typography>
+                        <br />
+                        <Typography variant="p" sx={{ color: textColor }}>
+                                Ayudanos a seguir ofreciendo un servicio gratuito y libre de publicidad.
+                        </Typography>
+                        <Divider />
 
-                        <Box mt={ 5 }>
-                            <Typography sx={{ fontWeight: 'bold' }} color={ textColor } mb={ 1 }> Paypal: </Typography>
+                    </Grid>
+
+                    <Grid container >
+                        <Grid item xs={ 12 } md={ 10 } mt={ 5 }>
+
+                            <Typography  variant="h6" color={ textColor }> Métodos de donación</Typography>
+
+                            <Divider />
+
+                            <Typography sx={{ fontWeight: 'bold' }} color={ textColor } mb={ 1 } mt={ 5 }> Paypal: </Typography>
 
                             <Link href="https://paypal.me/mimangapage?country.x=CL&locale.x=es_XC" underline="none" target="_blank" rel="noopener">
                                 <Button variant={ buttonVariant }> Donar por Paypal </Button>
                             </Link>
-                        </Box>
 
-                        <Box mt={ 10 }>
+                        </Grid>
+                                
+                        <Grid item xs={ 10 } sm={ 8 } md={ 6 } mt={ 5 }>
+
                             <Typography mb={ 2 } sx={{ fontWeight: 'bold' }} color={ textColor }> Criptomoneda: </Typography>
 
-                            <Typography fontSize={ 12 } mb={ 1 } color={ textColor }> USDT / BEP20 </Typography>
-                            
-                            <Grid container>
-                                <Grid item xs={ 10 }  >
-                                    <Typography sx={{ borderBlockEnd: '0.5px solid gray' }} color={ textColor }> { criptoUSDT } </Typography>
-                                </Grid>
-                                <Grid item xs={ 2 } mt={ -1 } >
-                                    <Tooltip title="Copiar">
-                                        <IconButton onClick={ () => onCopyClick( criptoUSDT ) } >
-                                            <ContentCopyIcon sx={{ fontSize:'25px' }} color={ ( styleMode === 'white' ) ? "action" : "warning"} />
-                                        </IconButton>
-                                    </Tooltip>
-                                </Grid>
-                            </Grid>
-                            
-                        </Box>
+                            <Typography fontSize={ 12 } mb={ 1 } color={ textColor } noWrap > USDT / BEP20 </Typography>
 
-                        <Box mt={ 3 }>
+                            <Typography sx={{ borderBlockEnd: '0.5px solid gray' }} color={ textColor } noWrap> { criptoUSDT } </Typography>
 
+                        </Grid>
+
+                        <Grid item xs={ 2 } sm={ 4 }  mt={ 12 } >
+                            <Tooltip title="Copiar">
+                                <IconButton onClick={ () => onCopyClick( criptoUSDT ) } >
+                                    <ContentCopyIcon sx={{ fontSize:'25px' }} color={ ( styleMode === 'white' ) ? "action" : "warning" } />
+                                </IconButton>
+                            </Tooltip>
+                        </Grid>
+
+                        <Grid item xs={ 10 } sm={ 8 } md={ 6 } mt={ 2 } >
                             <Typography fontSize={ 12 } mb={ 1 } color={ textColor }> BTC / BEP20 </Typography>
+                            <Typography sx={{ borderBlockEnd: '0.5px solid gray' }} color={ textColor } noWrap> { criptoBTC } </Typography>
+                        </Grid>
 
-                            <Grid container>
-                                <Grid item xs={ 10 }  >
-                                    <Typography sx={{ borderBlockEnd: '0.5px solid gray' }} color={ textColor }> { criptoBTC } </Typography>
-                                </Grid>
-                                <Grid item xs={ 2 } mt={ -1 } >
-                                    <Tooltip title="Copiar">
-                                        <IconButton onClick={ () => onCopyClick( criptoBTC ) } >
-                                            <ContentCopyIcon sx={{ fontSize:'25px' }} color={ ( styleMode === 'white' ) ? "action" : "warning"} />
-                                        </IconButton>
-                                    </Tooltip>
-                                </Grid>
-                            </Grid>
-                        </Box>
-                        <Snackbar open={open} autoHideDuration={2000} onClose={ () => setOpen(false) }>
-                            <Alert onClose={ () => setOpen(false) } severity="success" sx={{ width: '100%' }}>
-                                Copiado correctamente.
-                            </Alert>
-                        </Snackbar>             
-
+                        <Grid item xs={ 2 } sm={ 4 } mt={ 4 } >
+                            <Tooltip title="Copiar">
+                                <IconButton onClick={ () => onCopyClick( criptoBTC ) } >
+                                    <ContentCopyIcon sx={{ fontSize:'25px' }} color={ ( styleMode === 'white' ) ? "action" : "warning"} />
+                                </IconButton>
+                            </Tooltip>
+                        </Grid>
                     </Grid>
-
-                    <Grid item xs={ 12 } md={ 5 } align="center" >
-                        
-                        <img style={{ maxWidth: '100%', maxHeight:'500px' }} src="https://res.cloudinary.com/dmuswnvaf/image/upload/v1676238189/MyManga/assets/donate_cmsajq.webp" />
-
-                    </Grid>
+                    
                 </Grid>
 
-                
-                
+                <Snackbar open={open} autoHideDuration={2000} onClose={ () => setOpen(false) }>
+                    <Alert onClose={ () => setOpen(false) } severity="success" sx={{ width: '100%' }}>
+                        Copiado correctamente.
+                    </Alert>
+                </Snackbar>             
             </Grid>
+
         </MangaLayout>
     )
 
