f34be910ade8cfc728656ad176350e3029145764
Oscar Alcazar <oscar.alcazar.lobos@gmail.com>
2023-02-15 19:24:57 -0300

    TitleMangas.jsx: Set text responsive

--- src/mangawatcher/components/Public/TitleMangas.jsx
+++ src/mangawatcher/components/Public/TitleMangas.jsx
@@ -18,19 +18,17 @@ export const TitleMangas = () => {
     return (
         <Grid container justifyContent='center'  textAlign='center' pb={ 2 } className="animate__animated animate__fadeIn">
 
-            
+                
                 <Grid item  xs={ 10 } p={ 1 } minHeight='50px' className="box-shadow" sx={{ paddingLeft: { md: '150px', sm: '75px', xs: '0px', lg: '250px',  } , borderTop: '1px solid', borderColor: `${ styleMode }.secondary` , borderBottomLeftRadius:'10px', backgroundColor: `${ styleMode }.primary` }} >
-                    
-                    <Typography variant='h6' mt={ 1 } sx={{ color: `colors.gray.claro`, fontWeight: 'bold' }} >
+                    <Typography mt={ 1 } sx={{ color: `colors.gray.claro`, fontWeight: 'bold', fontSize: { xs: "16px", md: "20px" } }} >
                         { `${ manga.nombre }  ${ capitulo } Español - ${ titulo } `}			
                     </Typography>
-
                 </Grid>
 
                 <Grid item xs={ 2 } minHeight='50px' className="box-shadow" sx={{ borderTop: '1px solid', borderColor: `${ styleMode }.secondary`, borderBottomRightRadius:'10px', display:'flex' }} bgcolor={ `${ styleMode }.primary` } alignItems='center' justifyContent='center' >
                     <Tooltip title='Capítulos'>
                         <IconButton onClick={ onOpenModal }>
-                            <PinIcon sx={{ fontSize: '33px' }} className='botonesNavbar' />
+                            <PinIcon sx={{ fontSize: { xs: '27px', sm: '33px' } }} className='botonesNavbar' />
                         </IconButton>
                     </Tooltip>
                 </Grid>
